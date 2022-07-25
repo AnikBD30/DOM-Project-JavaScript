5406947eefa8d2da46d1d951e5530f7589f4854b
AnikBD30 <anikshutradharbd@gmail.com>
2022-07-25 11:48:49 +0600

    Dynamic Tost message

--- app.js
+++ app.js
@@ -2,9 +2,13 @@
  * Project requirement:
  * -change the background color by generating random hex color by clicking a button 
  * - Also display the hex code to a disabled input field 
+ *  * - Add a toast message when copied
  */
 //steps 
 //step-1 create onload handler 
+
+//Globals 
+let div = null; 
 window.onload = () =>{
      main(); 
 }
@@ -21,7 +25,12 @@ function main(){
 
   copyBtn.addEventListener('click', function(){
 	navigator.clipboard.writeText(output.value);
-  })
+	if(div != null){
+		div.remove()
+		div = null; 
+	}
+	generateToastMessage(`${output.value} Copied`)
+  }); 
 }
 
 //step-2 random color generator function 
@@ -34,7 +43,26 @@ function generateHexColor(){
 
 }
 
+function  generateToastMessage(msg){
+ div = document.createElement('div')
+div.innerText = msg; 
+div.className ='toast-message toast-message-slide-in'
+div.addEventListener('click', function(){
+	div.classList.remove('toast-message-slide-in'); 
+	div.classList.add('toast-message-slide-out'); 
+	div.addEventListener('animationend', function(){
+		div.remove(); 
+		div=null; 
+	}); 
+})
+
+document.body.appendChild(div);
+
+}
 //step-3 collect all necessary references 
 
 //step-5 handle the click event 
+// Step 6 - activate toast message
+// Step 7 - create a dynamic toast message
 
+// step 8 - clear toast message
